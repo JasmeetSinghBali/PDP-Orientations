9e647ac58f5cb218103ed432b161be0d59b9129d
Jasmeet-1998 <devs.us.1984@gmail.com>
2022-06-26 16:51:45 +0530

    views: request handlers in django

--- warehousefront/playground/views.py
+++ warehousefront/playground/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+# Create your views(request handler) here.
+def I_am_reqHandler(request):
+    # can be used to retreive data from DB
+    # transform data
+    # send email
+    return HttpResponse('Hello World')
