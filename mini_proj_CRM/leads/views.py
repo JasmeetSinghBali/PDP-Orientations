57b6318886dabf593b316264823bbbf46c112ada
Jasmeet-1998 <jasmeetbali.dev.2021@gmail.com>
2022-07-08 12:23:11 +0530

    📝:IMPlooping over context & display in unordlist

--- mini_proj_CRM/leads/views.py
+++ mini_proj_CRM/leads/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+# 📝 .models refers to relative import as we are inside the lead app
+from .models import Lead
 
 # Create your views here.
 def home_page(request):
-    context = {
-        "name": "Joe",
-        "age": 34
+    # where leads is a list of object
+    leads = Lead.objects.all()
+    context ={
+        "leads": leads
     }
     #return HttpResponse("Hello world")
     # returning a html page as template when this route is hit
